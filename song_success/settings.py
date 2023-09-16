6eae76ef4c92e45a8deaa7a5f6817c29bb79b6ce
Daniel Reyes <df.reyesr@uniandes.edu.co>
2023-09-16 02:18:39 -0500

    Corrected image missing

--- song_success/settings.py
+++ song_success/settings.py
@@ -26,7 +26,7 @@ MODEL_PATH = os.path.join(BASE_DIR, 'models/songs_success_predictor.pkl')
 SECRET_KEY = 'django-insecure-nru2s@ni(kgz-kfq)^qza5tvn=tvt)@ta8zlcb0r*d2nv)1@r2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 # Default primary key field type
