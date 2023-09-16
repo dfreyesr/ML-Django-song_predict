64a43053fd4d9181cdab97ba2b45ed7498041a7d
Daniel Reyes <df.reyesr@uniandes.edu.co>
2023-09-16 02:28:16 -0500

    Solved image issue finally
    Ready to deploy

--- song_success/settings.py
+++ song_success/settings.py
@@ -26,7 +26,7 @@ MODEL_PATH = os.path.join(BASE_DIR, 'models/songs_success_predictor.pkl')
 SECRET_KEY = 'django-insecure-nru2s@ni(kgz-kfq)^qza5tvn=tvt)@ta8zlcb0r*d2nv)1@r2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,9 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
